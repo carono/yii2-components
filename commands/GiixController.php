a9effc8c60cefb0d58bc7ff19ca0cfff6b6213a0
carno <spamer2000@list.ru>
2015-12-18 00:34:24 +0500

    refs

--- commands/GiixController.php
+++ commands/GiixController.php
@@ -4,6 +4,10 @@ namespace carono\components\commands;
 
 
 use schmunk42\giiant\commands\BatchController;
+use yii\gii\CodeFile;
+use yii\gii\generators\module\Generator;
+use yii\gii\Module;
+use yii\gii\console\GenerateController;
 
 class GiixController extends BatchController
 {
@@ -12,4 +16,103 @@ class GiixController extends BatchController
 	public $defaultAction = 'models';
 	public $interactive = false;
 	public $template = 'caronoModel';
+
+	public function beforeAction($action)
+	{
+		\Yii::$app->controller->module = \Yii::$app->getModule('gii');
+		return parent::beforeAction($action);
+	}
+
+	public function actionCreateModule($id)
+	{
+		/**
+		 * @var CodeFile $code
+		 */
+		$generator = new Generator();
+		$generator->moduleID = $id;
+		$generator->moduleClass = 'app\modules\\' . strtolower($id) . '\\' . ucfirst($id) . "Module";
+		foreach ($generator->generate() as $code) {
+			$code->save();
+		};
+		$this->stdout("Module '$id' generated\n");
+		$this->stdout("Add to modules: '$id' => '{$generator->moduleClass}'\ns");
+	}
+
+	public function actionCreateController($id)
+	{
+		/**
+		 * @var CodeFile $code
+		 */
+		$id = str_replace('/', '\\', $id);
+		$arr = explode('\\', $id);
+		$generator = new \yii\gii\generators\controller\Generator();
+		if (count($arr) == 1) {
+			$c = ucfirst($arr[0]);
+			$generator->controllerClass = "app\\controllers\\{$c}Controller";
+			$generator->viewPath = "views\\" . strtolower($c);
+		} else {
+			$m = ucfirst($arr[0]);
+			$c = ucfirst($arr[1]);
+			$generator->controllerClass = "app\\modules\\{$m}\\controllers\\{$c}Controller";
+			$generator->viewPath = "modules\\{$m}\\views\\" . strtolower($c);
+		}
+		foreach ($generator->generate() as $code) {
+			$code->save();
+		};
+		$this->stdout("Controller '$c' created");
+	}
+
+	public function actionCreateAction($id, $p = null)
+	{
+		$id = str_replace('/', '\\', $id);
+		$arr = explode('\\', $id);
+		$file = '';
+		$c = '';
+		$a = '';
+		$fileView = '';
+		if (count($arr) == 3) {
+			$m = $arr[0];
+			$c = ucfirst($arr[1]);
+			$a = ucfirst($arr[2]);
+			$a1 = strtolower($a);
+			$file = \Yii::getAlias("@app/modules/$m/controllers/{$c}Controller.php");
+			$fileView = \Yii::getAlias("@app/modules/$m/views/{$c}/$a1.php");
+		} elseif (count($arr) == 2) {
+			$c = ucfirst($arr[0]);
+			$a = ucfirst($arr[1]);
+			$a1 = strtolower($a);
+			$file = \Yii::getAlias("@app/controllers/{$c}Controller.php");
+			$fileView = \Yii::getAlias("@app/views/{$c}/$a1.php");
+		} else {
+		}
+		if (file_exists($file)) {
+			$content = trim(file_get_contents($file));
+			if (strpos($content, "action" . $a . "(") === false) {
+				$code = $this->getActionTemplate($a, explode(",", $p));
+				$new = preg_replace("/}$/", $code . "\n}", $content);
+				file_put_contents($file, $new);
+			}
+			if (!file_exists($fileView)) {
+				$codeFile = new CodeFile($fileView, "<?php \n //$c/$a");
+				$codeFile->save();
+			}
+		}
+	}
+
+	protected function getActionTemplate($id, $params = [])
+	{
+		$id = ucfirst($id);
+		$view = strtolower($id);
+		$p = [];
+		foreach ($params as $value) {
+			$p[] = "$" . trim($value);
+		}
+		$p = join(', ', $p);
+		return <<<EOT
+    public function action$id($p)
+    {
+        return \$this->render('$view');
+    }
+EOT;
+	}
 }
