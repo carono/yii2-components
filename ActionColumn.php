995bed3afbbab36cffe77cf8cb7116f573f1cdb6
carno <spamer2000@list.ru>
2015-12-24 10:51:49 +0500

    refs

---
+++ ActionColumn.php
@@ -0,0 +1,77 @@
+<?php
+namespace carono\components;
+
+use Yii;
+
+class ActionColumn extends \yii\grid\ActionColumn
+{
+	public $text;
+	public $checkUrlAccess = true;
+
+	public function createUrl($action, $model, $key, $index)
+	{
+		if (method_exists($model, 'getUrl') && ($url = $model->getUrl($action))) {
+			return $url;
+		} else {
+			return parent::createUrl($action, $model, $key, $index);
+		}
+	}
+
+	private static function registerModifyScript($id, $iconSend, $iconRevert, $iconOk = "glyphicon-ok",
+		$iconError = 'glyphicon-remove', $iconWait = "glyphicon-time")
+	{
+		$iconSend = strpos($iconSend, 'fa-') !== false ? "fa " . $iconSend : "glyphicon " . $iconSend;
+		$iconRevert = strpos($iconRevert, 'fa-') !== false ? "fa " . $iconRevert : "glyphicon " . $iconRevert;
+		$iconOk = strpos($iconOk, 'fa-') !== false ? "fa " . $iconOk : "glyphicon " . $iconOk;
+		$iconError = strpos($iconError, 'fa-') !== false ? "fa " . $iconError : "glyphicon " . $iconError;
+		$iconWait = strpos($iconWait, 'fa-') !== false ? "fa " . $iconWait : "glyphicon " . $iconWait;
+		$script
+			= <<<EOT
+    $('[ajax-link=$id]').on('click', function () {
+    	var elem=$(this);
+		$.ajax({
+			type: "POST",
+			url: $(this).attr('href'),
+			dataType: 'json',
+			async: true,
+			success: function (data) {
+				if (elem.attr('revert-url') == elem.attr('href')){
+					elem.find('.glyphicon, .fa').attr('class','').addClass('$iconSend');
+					elem.attr('href', elem.attr('send-url'));
+				}else if(elem.attr('send-url') == elem.attr('href')){
+					elem.find('.glyphicon, .fa').attr('class','').addClass('$iconRevert');
+					elem.attr('href', elem.attr('revert-url'));
+				}else{
+					elem.find('.glyphicon, .fa').attr('class','').addClass('$iconOk').off('click');
+					elem.on('click',function(){return false;})
+				}
+				if (data['message']) {
+					$(elem).parent().tooltip({
+						title: data['message'],
+						placement: 'right',
+						trigger:'hover',
+						delay: {show: 500, hide: 500}
+					}).tooltip('show').on('hidden.bs.tooltip',function(){ $(this).tooltip('destroy') });
+				}
+			},
+			beforeSend: function (){
+				elem.find('.glyphicon, .fa').attr('class','').addClass('$iconWait');
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				elem.find('.glyphicon, .fa').attr('class','').addClass('$iconError');
+				if (jqXHR.responseJSON['message']) {
+					$(elem).parent().tooltip({
+						title:jqXHR.responseJSON['message'],
+						placement : 'right',
+						trigger:'hover',
+						delay: {show: 500, hide: 500}
+					}).tooltip('show').on('hidden.bs.tooltip',function(){ $(this).tooltip('destroy') });
+				}
+			}
+		});
+        return false;
+    })
+EOT;
+		Yii::$app->getView()->registerJs($script);
+	}
+}
--- CurrentUser.php
+++ CurrentUser.php
@@ -1,19 +1,105 @@
 <?php
-/**
- * User: Карно
- * Date: 02.11.2015
- * Time: 13:54
- */
 
 namespace carono\components;
 
 
 use app\models\User;
+use yii\base\Model;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 
 class CurrentUser
 {
+	public static $className = 'app\models\User';
 
+	/**
+	 * @param Model|string $message
+	 */
+	public static function setFlashError($message)
+	{
+		if ($message instanceof Model) {
+			$message = Html::errorSummary($message);
+		}
+		self::setFlash('error', $message);
+	}
+
+	/**
+	 * @param $message
+	 */
+	public static function setFlashSuccess($message)
+	{
+		self::setFlash('success', $message);
+	}
+
+	/**
+	 * @param $message
+	 */
+	public static function setFlashWarning($message)
+	{
+		self::setFlash('warning', $message);
+	}
+
+	/**
+	 * @param $message
+	 */
+	public static function setFlashInfo($message)
+	{
+		self::setFlash('info', $message);
+	}
+
+	/**
+	 * @param null $key
+	 *
+	 * @return string
+	 */
+	public static function showFlash($key = null)
+	{
+		$session = \Yii::$app->getSession();
+		if (!$key) {
+			$out = '';
+			foreach ($session->getAllFlashes(false) as $key => $value) {
+				$out .= self::showFlash($key);
+			}
+			return $out;
+		} else {
+			switch ($key) {
+				case "success":
+					$htmlOptions = ["class" => "alert alert-success"];
+					break;
+				case "error":
+					$htmlOptions = ["class" => "alert alert-danger"];
+					break;
+				case "info":
+					$htmlOptions = ["class" => "alert alert-info"];
+					break;
+				case "warning":
+					$htmlOptions = ["class" => "alert alert-warning"];
+					break;
+				default:
+					$htmlOptions = ["class" => "alert alert-info"];
+			}
+			if ($session->hasFlash($key)) {
+				return Html::tag('div', $session->getFlash($key), $htmlOptions);
+			}
+		};
+	}
+
+	/**
+	 * @param $name
+	 * @param $message
+	 */
+	public static function setFlash($name, $message)
+	{
+		if (\Yii::$app->getSession()) {
+			\Yii::$app->getSession()->setFlash($name, $message);
+		}
+	}
+
+	/**
+	 * @param null $user
+	 *
+	 * @return bool
+	 */
 	public static function isMe($user = null)
 	{
 		return self::user($user)->id == self::getId(true);
