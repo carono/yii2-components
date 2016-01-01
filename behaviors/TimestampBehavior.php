ac4d2b39e72ca8157b0fa4451d47234d6362b68e
carno <spamer2000@list.ru>
2016-01-02 00:34:41 +0500

    refs

--- behaviors/TimestampBehavior.php
+++ behaviors/TimestampBehavior.php
@@ -3,9 +3,12 @@
 namespace carono\components\behaviors;
 
 
+use yii\db\ActiveRecord;
+
 class TimestampBehavior extends \yii\behaviors\TimestampBehavior
 {
 	public $onChange;
+	public $toTime;
 
 	protected function getValue($event)
 	{
@@ -17,6 +20,24 @@ class TimestampBehavior extends \yii\behaviors\TimestampBehavior
 			}
 		} else {
 			return parent::getValue($event);
+		}
+	}
+
+	public function wasUpdated()
+	{
+		$attr = null;
+		foreach ($this->attributes as $event => $attributes) {
+			if ($event == ActiveRecord::EVENT_BEFORE_UPDATE) {
+				$attr = is_array($attributes) ? $attributes[0] : $attributes;
+			}
+		}
+		if ($attr) {
+			$value = $this->owner->{$attr};
+			$time = $this->toTime !== null ? call_user_func($this->toTime, $value) : strtotime($value);
+			$dt = new \DateTime("now", new \DateTimeZone(\Yii::$app->timeZone));
+			$dt->setTimestamp($time);
+			$dt2 = new \DateTime("now", new \DateTimeZone(\Yii::$app->timeZone));
+			return $dt2->getTimestamp() - $dt->getTimestamp();
 		}
 	}
 }
