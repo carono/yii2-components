80ddb6fbba39b8a29f75a1c9a246a904c0561025
carno <spamer2000@list.ru>
2016-01-13 16:11:02 +0500

    nav

---
+++ Nav.php
@@ -0,0 +1,49 @@
+<?php
+namespace carono\components;
+
+class Nav extends \yii\bootstrap\Nav
+{
+
+	public function init()
+	{
+		$this->items = $this->filterItems($this->items);
+		parent::init();
+	}
+
+	/**
+	 * @param $items
+	 *
+	 * @return array
+	 */
+	public function filterItems($items)
+	{
+		$newItems = [];
+		foreach ($items as $item) {
+			if ($this->checkItemAccess($item)) {
+				if (isset($item["items"])) {
+					$item["items"] = $this->filterItems($item["items"]);
+					if (!$item["items"]) {
+						continue;
+					}
+				}
+				$newItems[] = $item;
+			}
+		}
+		return $newItems;
+	}
+
+	/**
+	 * @param $item
+	 *
+	 * @return bool
+	 */
+	protected function checkItemAccess($item)
+	{
+		if (isset($item["url"])) {
+			if (!RoleManager::checkAccessByUrl($item["url"])) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
