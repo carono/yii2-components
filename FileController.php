85ac23819280462d9b9dde30e3a6cced7aabc89e
carno <spamer2000@list.ru>
2016-01-21 09:54:17 +0500

    File controller

---
+++ FileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace carono\components;
+
+
+use yii\web\HttpException;
+
+class FileController extends \yii\web\Controller
+{
+	protected function out($path, $name = null)
+	{
+		if (file_exists($path)) {
+			$content = file_get_contents($path);
+			if ($this->beforeSend($path, $name)) {
+				\Yii::$app->response->sendContentAsFile($content, $name);
+			} else {
+				throw new HttpException(500, 'Download is interrupted');
+			}
+			$this->afterSend($path, $name);
+		} else {
+			throw new HttpException(404, 'File not found');
+		}
+	}
+
+	public function beforeSend($path, $name)
+	{
+		return true;
+	}
+
+	public function afterSend($path, $name)
+	{
+	}
+}
