1c8e8f34e9f2d4ec1c8278104e7bd054ba0ea674
carno <spamer2000@list.ru>
2016-03-02 15:37:36 +0500

    refs

--- migrations/m160222_202733_company.php
+++ migrations/m160222_202733_company.php
@@ -5,31 +5,31 @@ use carono\components\Migration;
 
 class m160222_202733_company extends Migration
 {
-	public function tables()
-	{
-		return [
-			"company" => [
-				"id"            => self::primaryKey(),
-				"name"          => self::string(),
-				"full_name"     => self::string(),
-				"ogrn"          => self::string(),
-				"inn"           => self::string(),
-				"kpp"           => self::string(),
-				"okpo"          => self::string(),
-				"founding_date" => self::dateTime(),
-				"created"       => self::dateTime(),
-				"updated"       => self::dateTime(),
-			]
-		];
-	}
+    public function tables($array = [])
+    {
+        return [
+            "company" => [
+                "id"            => self::primaryKey(),
+                "name"          => self::string(),
+                "full_name"     => self::string(),
+                "ogrn"          => self::string(),
+                "inn"           => self::string(),
+                "kpp"           => self::string(),
+                "okpo"          => self::string(),
+                "founding_date" => self::dateTime(),
+                "created"       => self::dateTime(),
+                "updated"       => self::dateTime(),
+            ]
+        ];
+    }
 
-	public function safeUp()
-	{
-		$this->upTables();
-	}
+    public function safeUp()
+    {
+        $this->upTables();
+    }
 
-	public function safeDown()
-	{
-		$this->downTables();
-	}
+    public function safeDown()
+    {
+        $this->downTables();
+    }
 }
