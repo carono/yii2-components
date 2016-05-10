55cbedcacb9e4b91996dae983412d1883e5c4d4b
carno <spamer2000@list.ru>
2016-05-10 19:27:19 +0500

    refs

--- FileController.php
+++ FileController.php
@@ -7,27 +7,27 @@ use yii\web\HttpException;
 
 class FileController extends \yii\web\Controller
 {
-	protected function out($path, $name = null)
-	{
-		if (file_exists($path)) {
-			$content = file_get_contents($path);
-			if ($this->beforeSend($path, $name)) {
-				\Yii::$app->response->sendContentAsFile($content, $name);
-			} else {
-				throw new HttpException(500, 'Download is interrupted');
-			}
-			$this->afterSend($path, $name);
-		} else {
-			throw new HttpException(404, 'File not found');
-		}
-	}
+    protected function out($path, $name = null, $options = [])
+    {
+        if (file_exists($path)) {
+            $content = file_get_contents($path);
+            if ($this->beforeSend($path, $name)) {
+                \Yii::$app->response->sendContentAsFile($content, $name, $options);
+            } else {
+                throw new HttpException(500, 'Download is interrupted');
+            }
+            $this->afterSend($path, $name);
+        } else {
+            throw new HttpException(404, 'File not found');
+        }
+    }
 
-	public function beforeSend($path, $name)
-	{
-		return true;
-	}
+    public function beforeSend($path, $name)
+    {
+        return true;
+    }
 
-	public function afterSend($path, $name)
-	{
-	}
+    public function afterSend($path, $name)
+    {
+    }
 }
